62e2f00c04d4e27117827636d7c17ec0ea472980
Youssef AlTabei <y.mamdouh13@gmail.com>
2025-01-30 23:10:23 +0200

    Syncing

--- Rook.py
+++ Rook.py
@@ -1,6 +1,12 @@
 from Pieces import Piece
-
-
+from Pieces import Piece, PinStatus
 class Rook(Piece):
-    def __init__(self, color):
+    def __init__(self, color, index):
         super().__init__(color)
+        self.index = index
+        self.has_moved = False
+        self.pin_state = PinStatus.NOT_PINNED
+    def check_legal_moves(self):
+        legal_moves = []
+        if self.pin_state = PINNED_DIAG:
+            
