c23c5d1e7a59b0ead2238c9059f0e720a7d4bdc6
Youssef AlTabei <y.mamdouh13@gmail.com>
2025-01-30 23:43:35 +0200

    Implemented Rook Class

--- Rook.py
+++ Rook.py
@@ -1,5 +1,6 @@
 from Pieces import Piece
 from Pieces import PinStatus
+import Empty
 class Rook(Piece):
     def __init__(self, color, index):
         super().__init__(color)
@@ -8,5 +9,23 @@ class Rook(Piece):
         self.pin_state = PinStatus.NOT_PINNED
     def check_legal_moves(self):
         legal_moves = []
-        if self.pin_state == PinStatus.PINNED_DIAG:
-            pass
+        if self.pin_state != PinStatus.PINNED_DIAG:
+            if self.pin_state != PinStatus.PINNED_HORZ:
+                temp = self.index + 8
+                temp2 = self.index - 8
+                while isinstance(super().arr[temp], Empty) and temp <= 64 and temp >=0:
+                    legal_moves.append(temp)
+                    temp += 8         
+                while isinstance(super().arr[temp2], Empty) and temp2 <= 64 and temp2 >=0:
+                    legal_moves.append(temp2)
+                    temp2 -= 8   
+            if self.pin_state != PinStatus.PINNED_VERT:
+                temp = self.index + 1
+                temp2 = self.index - 1
+                while isinstance(super().arr[temp], Empty) and temp % 8 != 0:
+                    legal_moves.append(temp)
+                    temp += 1
+                while isinstance(super().arr[temp2], Empty) and temp2 % 8 != 7:
+                    legal_moves.append(temp2)
+                    temp2 -= 1
+        return legal_moves
