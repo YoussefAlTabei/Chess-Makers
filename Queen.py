21526a0184e13f7e7ea79977e1f5632177e29c83
Youssef AlTabei <y.mamdouh13@gmail.com>
2025-02-01 12:36:16 +0200

    implemtened Knight,bishop and Queen classes

--- Queen.py
+++ Queen.py
@@ -1,6 +1,16 @@
 from Pieces import Piece
-
-
-class Queen(Piece):
-    def __init__(self, color):
+import Empty
+import PinStatus as PS
+import Rook
+import Bishop
+class Queen(Rook, Bishop):
+    def __init__(self, color, index):
         super().__init__(color)
+        self.index = index
+        self.pin_state = PS.NOT_PINNED
+    def check_legal_queen(self):
+        legal_moves = []
+        legal_moves += self.check_legal_rook()
+        legal_moves += self.check_legal_bishop()
+        return legal_moves
+            
